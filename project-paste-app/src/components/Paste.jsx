a0e79d6b65cec604b372e9e7d4795c26d9e84b2f
shashibaranwal <shashibaranwal09@gmail.com>
2025-01-24 22:52:49 +0545

    paste section and button added

--- project-paste-app/src/components/Paste.jsx
+++ project-paste-app/src/components/Paste.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Paste = () => {
+
+    const pastes = useSelector((state) => state.paste.pastes);
+    const [searchTerm, setSearchTerm] = useState('');
+    const dispatch = useDispatch();
+
+    const filteredData = pastes.filter((paste) => paste.title.toLowerCase().includes(searchTerm.toLocaleLowerCase()));
+
+
+
   return (
     <div>
-      All pastes
+      <input type="search" className='py-2 px-4 rounded-md min-w-[35rem]'
+        placeholder='search here'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+
+      <div className='flex flex-col gap-5 mt-5'>
+        {
+            filteredData.length > 0 &&
+            filteredData.map(
+                (paste) => {
+                    return (
+                        <div className='border'>
+                            <div>
+                                {paste.title}
+                            </div>
+                            <div>
+                                {paste.content}
+                            </div>
+                            <div className='flex flex-row gap-4 place-content-evenly mt-4'>
+                                <button>
+                                    Edit
+                                </button>
+                                <button>
+                                    View
+                                </button>
+                                <button>
+                                    Delete
+                                </button>
+                                <button>
+                                    Copy
+                                </button>
+                                <button>
+                                    Share
+                                </button>
+                            </div>
+                            <div>
+                                {paste.createdAt}
+                            </div>
+                        </div>
+                    )
+                }
+            )
+        }
+      </div>
     </div>
   )
 }
